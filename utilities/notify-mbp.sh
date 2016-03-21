94cbba7f20707311f4fb72f4c4f9461dbf52ed06
Alex Torok <Alex.Torok@jdsu.com>
2016-03-21 18:49:06 -0400

    features

--- utilities/notify-mbp.sh
+++ utilities/notify-mbp.sh
@@ -1,2 +1,2 @@
 SSH_IP=`echo $SSH_CONNECTION | cut -f1 -d" "`
-ssh -t -o StrictHostKeyChecking=no alextorok@$SSH_IP 2> /dev/null "/usr/local/bin/terminal-notifier -title '$1' -message '$2'"
+ssh -t -o StrictHostKeyChecking=no alextorok@$SSH_IP 2> /dev/null "/usr/local/bin/terminal-notifier -title '$1' -subtitle '$2' -message '$3'"
