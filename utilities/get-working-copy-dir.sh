9e74b5e52ab4a0007ac60e985cee196e47875592
Alex Torok <Alex.Torok@viavisolutions.com>
2019-02-13 16:32:07 -0500

    Add comments and space formatting to get-working-copy-dir

--- utilities/get-working-copy-dir.sh
+++ utilities/get-working-copy-dir.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-tmux list-panes -F "#{pane_current_path}" -t $1 | grep -Po '(?<=working-copy/)[^/]+' | sed 's_\(.*\)_\1_'
+# Lists all of the current paths for each pane in the current window
+# and gets the directory name after "working-copy". This is used to show
+# The git repo being worked on in the tmux window tabs
+tmux list-panes -F "#{pane_current_path}" -t $1 | grep -Po '(?<=working-copy/)[^/]+' | sed 's_\(.*\)_ \1_'
